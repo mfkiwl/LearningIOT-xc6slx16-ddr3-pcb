471e33b90f9c36b01248559ae20dcdaf42482d33
walker2048 <35227619@qq.com>
2019-11-18 16:31:30 +0800

    调整flash分区和引脚定义

--- AliOS-Things/board/stm32f401cc-lcd/aos/board.c
+++ AliOS-Things/board/stm32f401cc-lcd/aos/board.c
@@ -9,14 +9,14 @@ const hal_logic_partition_t hal_partitions[] =
         .partition_owner            = HAL_FLASH_EMBEDDED,
         .partition_description      = "Application",
         .partition_start_addr       = 0x08000000,
-        .partition_length           = 0x5C000, //368k bytes
+        .partition_length           = 0x20000, //128k bytes
         .partition_options          = PAR_OPT_READ_EN | PAR_OPT_WRITE_EN,
     },
     [HAL_PARTITION_PARAMETER_1] =
     {
         .partition_owner            = HAL_FLASH_EMBEDDED,
         .partition_description      = "PARAMETER1",
-        .partition_start_addr       = 0x0805C000,
+        .partition_start_addr       = 0x08020000,
         .partition_length           = 0x1000, // 4k bytes
         .partition_options          = PAR_OPT_READ_EN | PAR_OPT_WRITE_EN,
     },
@@ -24,7 +24,7 @@ const hal_logic_partition_t hal_partitions[] =
     {
         .partition_owner            = HAL_FLASH_EMBEDDED,
         .partition_description      = "PARAMETER2",
-        .partition_start_addr       = 0x0805D000,
+        .partition_start_addr       = 0x08021000,
         .partition_length           = 0x2000, //8k bytes
         .partition_options          = PAR_OPT_READ_EN | PAR_OPT_WRITE_EN,
     },
@@ -32,7 +32,7 @@ const hal_logic_partition_t hal_partitions[] =
     {
         .partition_owner           = HAL_FLASH_EMBEDDED,
         .partition_description     = "PARAMETER4",
-        .partition_start_addr      = 0x0805F000,
+        .partition_start_addr      = 0x08023000,
         .partition_length          = 0x1000, //4k bytes
         .partition_options         = PAR_OPT_READ_EN | PAR_OPT_WRITE_EN,
     },
@@ -40,8 +40,8 @@ const hal_logic_partition_t hal_partitions[] =
     {
         .partition_owner            = HAL_FLASH_EMBEDDED,
         .partition_description      = "spiffs",
-        .partition_start_addr       = 0x08060000,
-        .partition_length           = 0x20000, //128k bytes
+        .partition_start_addr       = 0x08024000,
+        .partition_length           = 0x1C000, //112k bytes
         .partition_options          = PAR_OPT_READ_EN | PAR_OPT_WRITE_EN,
     },
 };
