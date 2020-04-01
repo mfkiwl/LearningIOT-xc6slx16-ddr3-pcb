abad77ee702557f9f7627850c31d44255b2c2bb9
walker2048 <35227619@qq.com>
2020-04-01 22:35:00 +0800

    添加约束

--- spartan6/uart/uart_top.vhd
+++ spartan6/uart/uart_top.vhd
@@ -2,12 +2,12 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity uart is
-    Port (tx:out BIT);
+    Port (tx1:out std_logic;
+    tx2:out std_logic);
 end uart;
 
 architecture Behavioral of uart is
-
 begin
-
-
+    tx1<='0';
+    tx2<='1';
 end Behavioral;
