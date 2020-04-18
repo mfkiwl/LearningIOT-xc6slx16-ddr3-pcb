5d6026e75d2c8151f732aa8354f9e16f021018b3
walker2048 <35227619@qq.com>
2020-04-18 16:06:10 +0800

    修改配置

--- spartan6/dcm&pll/dcm_top.vhd
+++ spartan6/dcm&pll/dcm_top.vhd
@@ -13,14 +13,16 @@ end dcm;
 architecture Behavioral of dcm is
     signal div_64_1 : std_logic_vector(5 downto 0) := "000000";
     signal div_64 : std_logic_vector(5 downto 0) := "000000";
-    signal clk_16 : std_logic;
-    signal clk_50 : std_logic;
+    signal clk_25 : std_logic;
+    signal clk_33 : std_logic;
+    signal clk_80 : std_logic;
     signal clk_100 : std_logic;
 
     component clock
         Port (  clk_in          :in     std_logic;
-                clk_out_16      :out    std_logic;
-                clk_out_50      :out    std_logic;
+                clk_out_25      :out    std_logic;
+                clk_out_33      :out    std_logic;
+                clk_out_80      :out    std_logic;
                 clk_out_100     :out    std_logic;
                 lock            :out    std_logic);
     end component;
@@ -28,14 +30,15 @@ architecture Behavioral of dcm is
 begin
     CLOCK_PLL : clock
     port map (  clk_in => clk_in,
-                clk_out_16 => clk_16,
-                clk_out_50 => clk_50,
+                clk_out_25 => clk_25,
+                clk_out_33 => clk_33,
+                clk_out_80 => clk_80,
                 clk_out_100 => clk_100,
                 lock => led0);
 
-    process (clk_16)
+    process (clk_25)
         begin
-            if rising_edge(clk_16) then
+            if rising_edge(clk_25) then
                 div_64_1 <= div_64_1 + 1;
                 if(div_64_1 = "000000") then
                     div_64 <= div_64 +1;
