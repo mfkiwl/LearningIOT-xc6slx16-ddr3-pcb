fac3ae8c4b3f07d394570049361d17281a2d780d
walker2048 <35227619@qq.com>
2020-04-29 13:07:36 +0800

    添加时钟

--- spartan6/dcm&pll/dcm_top.vhd
+++ spartan6/dcm&pll/dcm_top.vhd
@@ -6,15 +6,22 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 entity dcm is
     Port (  mcu,ext     :inout       std_logic_vector(5 downto 0);
             clk_in      :in          std_logic;
-            led0        :out         std_logic;
+            led0        :inout         std_logic;
             led1        :inout       std_logic);
 end dcm;
 
 architecture Behavioral of dcm is
-    signal div_65535    : integer range 0 to 65535 := 0;
-    signal clk_3        : std_logic;
-    signal clk_16       : std_logic;
-    signal clk_33       : std_logic;
+    signal div_1            : std_logic_vector(7 downto 0) := (others => '0');
+    signal div_2            : std_logic_vector(7 downto 0) := (others => '0');
+    signal clk_3            : std_logic;
+    signal clk_16           : std_logic;
+    signal clk_33           : std_logic;
+    signal clk_133          : std_logic;
+    signal clk_166          : std_logic;
+    signal clk_333          : std_logic;
+    signal clk_666          : std_logic;
+    signal low_clk_lock     : std_logic;
+    signal high_clk_lock    : std_logic;
 
     component low_clock
         Port (  clk_in          :in     std_logic;
@@ -24,22 +31,51 @@ architecture Behavioral of dcm is
                 lock            :out    std_logic);
     end component;
 
+    component high_clock
+        Port (  clk_in          :in     std_logic;
+                clk_out_133     :out    std_logic;
+                clk_out_166     :out    std_logic;
+                clk_out_333     :out    std_logic;
+                clk_out_666     :out    std_logic;
+                lock            :out    std_logic);
+    end component;
+
 begin
-    CLOCK_PLL : low_clock
+    LOW_CLOCK_PLL : low_clock
     port map (  clk_in => clk_in,
                 clk_out_3 => clk_3,
                 clk_out_16 => clk_16,
                 clk_out_33 => clk_33,
-                lock => led0);
+                lock => low_clk_lock);
+                
+    HIGH_CLOCK_PLL : high_clock
+    port map (  clk_in => clk_33,
+                clk_out_133 => clk_133,
+                clk_out_166 => clk_166,
+                clk_out_333 => clk_333,
+                clk_out_666 => clk_666,
+                lock => high_clk_lock);
 
-    process (clk_3)
+    L1: process (clk_3)
         begin
             if rising_edge(clk_3) then
-                div_65535 <= div_65535 + 1;
-                if (div_65535 = 65535) then
-                    div_65535 <= 0;
+                div_1 <= div_1 + 1;
+                if (div_1 = 255) then
+                    div_1 <= (others => '0');
                     led1 <= not led1;
                 end if;
             end if;
     end process;
+
+    L0: process (clk_133)
+    begin
+        if rising_edge(clk_133) then
+            div_2 <= div_2 + 1;
+            if (div_2 = 255) then
+                div_2 <= (others => '0');
+                led0 <= not led0;
+            end if;
+        end if;
+    end process;
+
 end Behavioral;
